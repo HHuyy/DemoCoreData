6657236a2c080f80a9afde3c49ca7f1eac22bdbe
Đừng xóa <nomnom1204@gmail.com>
2018-07-03 09:08:07 +0700

    init

--- DemoCoreData/ViewController.swift
+++ DemoCoreData/ViewController.swift
@@ -7,14 +7,26 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor.gray
         // Do any additional setup after loading the view, typically from a nib.
+        guard let entity = try? AppDelegate.context.fetch(Entity.fetchRequest()) as? [Entity] else { return  }
+        textField.text = entity?.last?.name
     }
 
+    @IBAction func ButtonTap(_ sender: UIButton) {
+        guard textField.text != nil else {return}
+        let entity = Entity(context: AppDelegate.context)
+        entity.name = textField.text
+        AppDelegate.saveContext()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
